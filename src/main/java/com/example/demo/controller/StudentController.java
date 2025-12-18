b69c9733d73cfeb02b78b5675abb4f01d79bd98b
bala-amypo <bala@amypo.in>
2025-12-18 09:27:24 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.Service.StudentService;
 import java.util.*;
 import jakarta.validation.constraints.*;
+import jakarta.validation.valid;
 @RestController
 public class StudentController{
     @Autowired
